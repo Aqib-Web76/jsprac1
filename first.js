1ed0676302e449a369846b9d9d875fd45fb55c47
Aqib-Web76 <117468357+Aqib-Web76@users.noreply.github.com>
2024-04-30 14:06:32 +0500

    Logical Operators

--- first.js
+++ first.js
@@ -10,5 +10,38 @@ console.log("the new value will be:", b);
 let c = 6;
 c = c + 5;
 
+// comparison operator
 console.log(c);
+if (a !== 5) {
+    console.log("you pass");
+} else {
+    console.log("fail");
+}
+
+//Logical Operator
+let x = 4;
+let y = 5;
+console.log("Using Logical AND below:");
+if (x == 4 && y == 5) {
+    console.log("pass");
+}
+else {
+    console.log("fail ");
+}
+console.log("Using Logical OR below:");
+
+let j = 9;
+let k = 8;
+if (j == 6  || k == 5) {
+    console.log("drink");
+}
+else {
+    console.log("eat");
+}
+console.log("Using Logical Not below:");
+let s = 4;
+let t = 3;
+console.log(!(a < b)); //if a is less then 4 then the condition is false but due to the ! it will give you tue ans
+
+
 
