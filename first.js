f41f726108a65923f799df23076d590364767c27
Aqib-Web76 <117468357+Aqib-Web76@users.noreply.github.com>
2024-04-30 13:01:11 +0500

    add new practice components

---
+++ first.js
@@ -0,0 +1,15 @@
+/*sole.log("operators");
+let a = 5;
+let b = 2;
+console.log("the result of a + b = ", a ** b);
+console.log("Uneray operators");*/
+//  this operators need only one operand
+ let a = 5;
+ let b = 2;
+console.log(--a);
+console.log(++a);
+console.log(b--);
+console.log(--b);
+// assignment operator
+
+
