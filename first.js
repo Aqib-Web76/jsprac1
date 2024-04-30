514c2441cc40ecd735a754643aff0aeb271f196b
Aqib-Web76 <117468357+Aqib-Web76@users.noreply.github.com>
2024-04-30 15:57:20 +0500

    Grade through if else

--- first.js
+++ first.js
@@ -1,3 +1,4 @@
+/*
 // Assignment operators
 let a = 4;
 a **= 7;
@@ -81,4 +82,25 @@ if (num % 2 === 0) {
     console.log("number is odd")
 
 }
+*/
+let marks = prompt("Entery your Marks:");
+if (marks >= 80 && marks <= 100) {
+    console.log("Grade A")
+} else if (marks >= 70 && marks <= 79) {
+    console.log("Grade B")
+
+}
+ else if (marks >= 60 && marks <= 69) {
+    console.log("Grade C")
+
+}
+ else if (marks >= 50 && marks <= 59) {
+    console.log("Grade D")
+
+}
+ else if (marks >= 0 && marks <= 49) {
+    console.log("Fail")
+
+}
+
 
