3b5b611165c44cc5885d4db5ed74062ff955a632
Aqib-Web76 <117468357+Aqib-Web76@users.noreply.github.com>
2024-05-02 12:33:09 +0500

    end of loop

--- first.js
+++ first.js
@@ -1,107 +1,142 @@
+// /*
+// // Assignment operators
+// let a = 4;
+// a **= 7;
+// let b = 5;
+// b %= 2;//b=b %2
+
+
+// console.log("the new value will be:", a);
+// console.log("the new value will be:", b);
+// let c = 6;
+// c = c + 5;
+
+// // comparison operator
+// console.log(c);
+// if (a !== 5) {
+//     console.log("you pass");
+// } else {
+//     console.log("fail");
+// }
+
+// //Logical Operator
+// let x = 4;
+// let y = 5;
+// console.log("Using Logical AND below:");
+// if (x == 4 && y == 5) {
+//     console.log("pass");
+// }
+// else {
+//     console.log("fail ");
+// }
+// console.log("Using Logical OR below:");
+
+// let j = 9;
+// let k = 8;
+// if (j == 6  || k == 5) {
+//     console.log("drink");
+// }
+// else {
+//     console.log("eat");
+// }
+// console.log("Using Logical Not below:");
+// let s = 4;
+// let t = 3;
+// console.log(!(a < b)); //if a is less then 4 then the condition is false but due to the ! it will give you tue ans
+
+
+// // for finding num is odd or even
+// let m = 45;
+// if(m%2 ==0){
+//     console.log(m, "is even");
+// } else {
+//     console.log("number is Odd")
+// }
+// // else if
+// let mode= "blue";
+// let color;
+// if (mode==="dark") {
+//     console.log(color = "black");
+// } else if (mode==="blue") {
+//     console.log(color = "blue");
+// }
+//  else if (mode==="pink") {
+//     console.log(color = "pink");
+// }
+// else {
+//     console.log(color = "white");
+// }
+// // Ternary Operator
+// let age = 45;
+// let h = age < 18 ? "adult" : "not adult";
+// console.log(h);
+
+// // alert and prompt
+// alert("go to prompt");
+
+// let num = prompt("enter value");
+// if (num % 2 === 0) {
+//     console.log("number is even")
+
+// } else {
+//     console.log("number is odd")
+
+// }
+// */
+// // let marks = prompt("Entery your Marks:");
+// // if (marks >= 80 && marks <= 100) {
+// //     console.log("Grade A")
+// // } else if (marks >= 70 && marks <= 79) {
+// //     console.log("Grade B")
+
+// // }
+// //  else if (marks >= 60 && marks <= 69) {
+// //     console.log("Grade C")
+
+// // }
+// //  else if (marks >= 50 && marks <= 59) {
+// //     console.log("Grade D")
+
+// // }
+// //  else if (marks >= 0 && marks <= 49) {
+// //     console.log("Fail")
+
+// // }
+
+// // for Loop
+// for (let i = 1; i <= 10; i++){
+//     console.log("Aqib is my friend");
+// }
+// // while loop:> initialization is put before while
+
+// let s = 0;
+// while (<= 6) {
+//     console
+// }
+
+// // do while loop:> it is used when code is execute at least one time even if the condition is false
+// /*let k = 2;
+// do {
+//     console.log("do while loop");
+//     k++
+// } while (k <= 9);*/
+
+//  for-of loop :>  it help to iterate loop to put loops on arrays and strings
+/*let str = "Aqib Shinwari Landikotal Saidge Khel";
+ for (let i of str) {
+     console.log(i);
+ }*/
+
+//  for-in loop :> it is used for objects as well as for arrays.
 /*
-// Assignment operators
-let a = 4;
-a **= 7;
-let b = 5;
-b %= 2;//b=b %2
+let student = {
+    Name: "aqib",
+    age: 20,
+    GPA: 3.8,
+    isPass: true,
+};
 
-
-console.log("the new value will be:", a);
-console.log("the new value will be:", b);
-let c = 6;
-c = c + 5;
-
-// comparison operator
-console.log(c);
-if (a !== 5) {
-    console.log("you pass");
-} else {
-    console.log("fail");
+for (let i in student) {
+    console.log(i,student[i]);
 }
-
-//Logical Operator
-let x = 4;
-let y = 5;
-console.log("Using Logical AND below:");
-if (x == 4 && y == 5) {
-    console.log("pass");
-}
-else {
-    console.log("fail ");
-}
-console.log("Using Logical OR below:");
-
-let j = 9;
-let k = 8;
-if (j == 6  || k == 5) {
-    console.log("drink");
-}
-else {
-    console.log("eat");
-}
-console.log("Using Logical Not below:");
-let s = 4;
-let t = 3;
-console.log(!(a < b)); //if a is less then 4 then the condition is false but due to the ! it will give you tue ans
-
-
-// for finding num is odd or even
-let m = 45;
-if(m%2 ==0){
-    console.log(m, "is even");
-} else {
-    console.log("number is Odd")
-}
-// else if
-let mode= "blue";
-let color;
-if (mode==="dark") {
-    console.log(color = "black");
-} else if (mode==="blue") {
-    console.log(color = "blue");
-}
- else if (mode==="pink") {
-    console.log(color = "pink");
-}
-else {
-    console.log(color = "white");
-}
-// Ternary Operator
-let age = 45;
-let h = age < 18 ? "adult" : "not adult";
-console.log(h);
-
-// alert and prompt
-alert("go to prompt");
-
-let num = prompt("enter value");
-if (num % 2 === 0) {
-    console.log("number is even")
-
-} else {
-    console.log("number is odd")
-
-}
-*/
-let marks = prompt("Entery your Marks:");
-if (marks >= 80 && marks <= 100) {
-    console.log("Grade A")
-} else if (marks >= 70 && marks <= 79) {
-    console.log("Grade B")
-
-}
- else if (marks >= 60 && marks <= 69) {
-    console.log("Grade C")
-
-}
- else if (marks >= 50 && marks <= 59) {
-    console.log("Grade D")
-
-}
- else if (marks >= 0 && marks <= 49) {
-    console.log("Fail")
-
-}
-
-
 
